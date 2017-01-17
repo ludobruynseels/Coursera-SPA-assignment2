c9e16491f242c1f267f65ac56343b6d423ce9530
Ludo <Ludo@ludobruynseels.com>
2017-01-17 23:21:44 +0100

    add service.
    Initialize list 'ToBuy'

--- app.js
+++ app.js
@@ -4,14 +4,33 @@
 angular.module('shoppingListModule', [])
     .controller('ToBuyController', ToBuyController)
     .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListService', ShoppingListService)
 
-    ToBuyController.$inject = ['$scope'];
-    function ToBuyController($scope) {
-        $scope.ToBuy = this;
+    ToBuyController.$inject = ['ShoppingListService'];
+    function ToBuyController(ShoppingListService) {
+        var ToBuy = this;
+
+        ToBuy.Items = ShoppingListService.getItemsToBuy();
     }
 
     AlreadyBoughtController.$inject = ['$scope'];
-    function AlreadyBoughtController($scope) {
-        $scope.Bought = this;
+    function AlreadyBoughtController(ShoppingListService) {
+        var Bought = this;
     }
+
+    function ShoppingListService() {
+        var service = this;
+        // List of shopping items
+        var itemsToBuy = [{name: 'Cookies', quantity: 10},
+            {name: 'Donuts', quantity: 15},
+            {name: 'Chips', quantity: 3},
+            {name: 'Bounty', quantity: 8},
+            {name: 'Milk', quantity: 2}
+            ];
+
+
+        service.getItemsToBuy = function () {
+            return itemsToBuy;
+        }
+    };
 })();
